1d39f2ab61eb1d089da555d026821190bb91431a
IsaacPrice <iprice25250@gmail.com>
2024-10-29 21:07:50 -0400

    initial commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+
+function App() 
+{
+	return (
+		<Box>
+			<Typography variant="h1">Homepage</Typography>
+		</Box>
+	);
+}
+
+
+export default App;
