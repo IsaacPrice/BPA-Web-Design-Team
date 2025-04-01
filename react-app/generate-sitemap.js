27dc31890151f6a7287b73d35f59335433539026
IsaacPrice <IPrice25250@gmail.com>
2025-03-31 22:13:19 -0400

    Update page for SEO

---
+++ react-app/generate-sitemap.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const routes = [
+  '/',
+  '/tours',
+  '/tour/:tourName',
+  '/merch',
+  '/merch/:merchName',
+  '/contact',
+  '/about',
+];
+
+const baseUrl = 'https://yourwebsite.com'; 
+
+function generateSitemapXml(routes, baseUrl) {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  
+  routes.forEach(route => {
+    xml += '  <url>\n';
+    xml += `    <loc>${baseUrl}${route}</loc>\n`;
+    xml += '    <changefreq>weekly</changefreq>\n';
+    xml += '    <priority>0.8</priority>\n';
+    xml += '  </url>\n';
+  });
+  
+  xml += '</urlset>';
+  return xml;
+}
+
+function saveSitemap(xml, outputPath) {
+  fs.writeFileSync(outputPath, xml);
+  console.log(`Sitemap has been generated at ${outputPath}`);
+}
+
+const sitemapXml = generateSitemapXml(routes, baseUrl);
+saveSitemap(sitemapXml, path.resolve('./public/sitemap.xml'));
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from './pages/Home';
 import { NavBar } from './pages/NavBar';
 import { theme } from './theme/theme';
+import { Footer } from './pages/Footer';
+import { Home } from './pages/Home';
 import { Tours } from './pages/Tour/Tours';
 import { MerchPage } from './pages/Merch/Merch';
 import { Contact } from './pages/Contact/Contact';
 import { About } from './pages/About';
 import { MerchItemPage } from './pages/Merch/MerchItem';
 import { TourDetails } from './pages/Tour/TourDetails';
-import { Footer } from './pages/Footer';
 
 
 function App() 
@@ -19,15 +19,15 @@ function App()
 			<BrowserRouter>
 				<NavBar />
 				<Footer>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/contact" element={<Contact />} />
-						<Route path="/merch" element={<MerchPage />} />
-						<Route path="/merch/:merchName" element={<MerchItemPage />} />
-						<Route path="/tours" element={<Tours />} />
-						<Route path="/tours/:tourName" element={<TourDetails />} />
-					</Routes>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/tours" element={<Tours />} />
+                        <Route path="/merch" element={<MerchPage />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/merch/:merchName" element={<MerchItemPage />} />
+                        <Route path="/tour/:tourName" element={<TourDetails />} />
+                    </Routes>
 				</Footer>
 			</BrowserRouter>
 		</ThemeProvider>
